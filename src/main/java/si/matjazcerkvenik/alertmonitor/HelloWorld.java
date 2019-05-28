8d5016be0e9c8ddadc80b345019534b30ee9eed7
matjaz <cerkvenik@iskratel.si>
2019-05-28 18:04:51 +0200

    initial commit

---
+++ src/main/java/si/matjazcerkvenik/alertmonitor/HelloWorld.java
@@ -0,0 +1,30 @@
+package main.java.si.matjazcerkvenik.alertmonitor;
+
+import javax.inject.Named;
+
+@Named
+public class HelloWorld {
+
+    private String firstName = "John";
+    private String lastName = "Doe";
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String showGreeting() {
+        return "Hello " + firstName + " " + lastName + "!";
+    }
+}
---
+++ src/main/java/si/matjazcerkvenik/alertmonitor/Start.java
@@ -0,0 +1,13 @@
+package main.java.si.matjazcerkvenik.alertmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Start {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Start.class, args);
+    }
+
+}
