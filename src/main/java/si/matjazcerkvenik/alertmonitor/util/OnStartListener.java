5d5ef6a67596c8d882ccff28ba17ad55dde17eba
matjaz99 <matjaz99@gmail.com>
2019-09-15 23:56:54 +0200

    update

--- src/main/java/si/matjazcerkvenik/alertmonitor/util/OnStartListener.java
+++ src/main/java/si/matjazcerkvenik/alertmonitor/util/OnStartListener.java
@@ -5,6 +5,8 @@ import si.matjazcerkvenik.alertmonitor.model.DAO;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import java.io.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
@@ -19,17 +21,26 @@ public class OnStartListener implements ServletContextListener {
 
         // read version file
         InputStream inputStream = servletContextEvent.getServletContext().getResourceAsStream("/WEB-INF/version.txt");
-        StringBuilder textBuilder = new StringBuilder();
-        try (Reader reader = new BufferedReader(new InputStreamReader
-                (inputStream, Charset.forName(StandardCharsets.UTF_8.name())))) {
-            int c = 0;
-            while ((c = reader.read()) != -1) {
-                textBuilder.append((char) c);
-            }
-            DAO.version = textBuilder.toString();
+        try {
+            DataInputStream dis = new DataInputStream(inputStream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(dis));
+            DAO.version = br.readLine();
+            dis.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+//        StringBuilder textBuilder = new StringBuilder();
+//        try (Reader reader = new BufferedReader(new InputStreamReader
+//                (inputStream, Charset.forName(StandardCharsets.UTF_8.name())))) {
+//            int c = 0;
+//            while ((c = reader.read()) != -1) {
+//                textBuilder.append((char) c);
+//            }
+//            DAO.version = textBuilder.toString();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
 //        DAO.getLogger().info("***********************************");
 //        DAO.getLogger().info("*                                 *");
@@ -39,10 +50,12 @@ public class OnStartListener implements ServletContextListener {
 //
 //        DAO.getLogger().info("VERSION=" + DAO.version);
 
+        System.out.println("IP_ADDR=" + getLocalIpAddress());
+
         // read environment variables
         Map<String, String> map = System.getenv();
         for (Map.Entry <String, String> entry: map.entrySet()) {
-//            DAO.getLogger().info(entry.getKey() + "=" + entry.getValue());
+            System.out.println(entry.getKey() + "=" + entry.getValue());
         }
 
     }
@@ -53,5 +66,13 @@ public class OnStartListener implements ServletContextListener {
 //        DAO.getLogger().info("#");
 //        DAO.getLogger().info("# Stopping Alertmonitor");
 //        DAO.getLogger().info("#\n\n\n");
+    }
+
+    public static String getLocalIpAddress() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            return "UnknownHost";
+        }
     }
 }
