bbf31b5712a92cd46391f56f5bd1bdb7e352612a
Matjaž Cerkvenik <matjaz99@gmail.com>
2025-06-10 23:03:24 +0200

    moved alerts.xhtml to alerts directory

---
+++ src/main/java/si/matjazcerkvenik/alertmonitor/web/uibeans/UiRedirectBean.java
@@ -0,0 +1,21 @@
+package si.matjazcerkvenik.alertmonitor.web.uibeans;
+
+import java.io.IOException;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+
+@Named("uiRedirectBean")
+@RequestScoped
+public class UiRedirectBean {
+    public void redirect() {
+        try {
+            FacesContext.getCurrentInstance()
+                        .getExternalContext()
+                        .redirect("/alertmonitor/alerts");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
